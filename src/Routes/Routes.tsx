940033b5300bd337dccae90352abe5c1d0c0f1bf
Kevin Fries <kevinsctfries@gmail.com>
2024-05-29 19:23:19 -0400

    changes Router to HashRouter

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createHashRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import App from "../App";
 import HomePage from "../pages/HomePage/HomePage";
 import AboutPage from "../pages/AboutPage/AboutPage";
