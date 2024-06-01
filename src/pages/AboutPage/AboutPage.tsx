463899a5b76b217330719ddd056bd49cc2418ec0
Kevin Fries <kevinsctfries@gmail.com>
2024-05-31 22:16:15 -0400

    minor css changes

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -10,6 +10,17 @@ const AboutPage = () => {
         nulla error vitae expedita molestias magni fuga possimus, porro quos
         animi.
       </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt eius,
+        alias expedita, aliquam, velit atque sit tempora earum repellat
+        dignissimos nesciunt esse ratione dolores blanditiis nemo adipisci sequi
+        ipsam molestiae!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A voluptas hic
+        ullam assumenda eligendi rem quibusdam, illo laudantium quasi modi amet
+        repellendus? Odio, porro laboriosam vero a iste minus harum.
+      </p>
     </div>
   );
 };
