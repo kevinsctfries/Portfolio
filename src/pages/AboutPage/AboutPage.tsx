da2e2c97e8921150ed4f38a91dfaae5db5ff76d6
Kevin Fries <kevinsctfries@gmail.com>
2024-06-10 16:32:06 -0400

    edited AboutPage, and added to ProjectsPage

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -4,23 +4,9 @@ const AboutPage = () => {
   return (
     <div className="about">
       <span>about me,</span>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab unde
-        asperiores odio iusto voluptatibus modi distinctio natus aperiam libero
-        nulla error vitae expedita molestias magni fuga possimus, porro quos
-        animi.
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt eius,
-        alias expedita, aliquam, velit atque sit tempora earum repellat
-        dignissimos nesciunt esse ratione dolores blanditiis nemo adipisci sequi
-        ipsam molestiae!
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A voluptas hic
-        ullam assumenda eligendi rem quibusdam, illo laudantium quasi modi amet
-        repellendus? Odio, porro laboriosam vero a iste minus harum.
-      </p>
+      <p>My name is Kevin Fries, and I'm a web developer.</p>
+      <p>I'm born and raised in California, but now live in North Carolina.</p>
+      <p>I have a strong passion for coding, especially for web development.</p>
     </div>
   );
 };
--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,14 +1,20 @@
 import "./projectspage.css";
+import typeTwo from "../../assets/type2.png";
+import typeTwoLogo from "../../assets/type2Logo.png";
 
 const ProjectsPage = () => {
   return (
     <div className="projects">
-      <span>my projects,</span>
+      <span>what I'm working on,</span>
+      <img src={typeTwoLogo} alt="" className="projectLogo" />
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto ex,
-        explicabo iure reprehenderit iusto error voluptate. Ea, nemo. Quo
-        incidunt quis provident id. Inventore nobis itaque, eveniet ab culpa
-        molestiae?
+        Currently, I'm working on an isometric MMO written in TypeScript and
+        using PixiJS.
+      </p>
+      <img src={typeTwo} alt="" className="projectImage" />
+      <p>
+        Type2 is, at the moment, the project title for the game and is likely to
+        change.
       </p>
     </div>
   );
