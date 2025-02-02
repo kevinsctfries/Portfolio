2aa6ac7e36b46e68223a72ab44a4c27176e4f469
Kevin Fries <kevinsctfries@gmail.com>
2025-02-01 21:34:05 -0500

    Major CSS changes, finished personal descriptions, added project

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -24,7 +24,10 @@ const HomePage = () => {
         <h2>hello,</h2>
         <p>
           My name is <span className="name">Kevin Fries</span>, and I like
-          making websites!
+          making websites! I am passionate about building user-friendly,
+          responsive, and performant websites. Over the years, I've honed my
+          skills in both front-end and back-end development, and I enjoy the
+          process of turning complex problems into simple, elegant solutions.
         </p>
       </div>
       <div className="section">
@@ -59,8 +62,11 @@ const HomePage = () => {
       <div className="section">
         <h2>about me,</h2>
         <p>
-          I am a web developer based in the Raleigh, NC area. Need to write more
-          here.
+          I am a web developer based in the Raleigh, NC area. I have a deep
+          passion for learning new technologies and continuously improving my
+          skills. In my free time, I enjoy exploring new frameworks, working on
+          my own projects, and staying up-to-date with the latest trends in web
+          development. When I'm not coding, you can find me playing piano
         </p>
       </div>
     </div>
--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,36 +1,38 @@
 import "./projectspage.css";
 import typeTwo from "../../assets/type2.png";
+import ohNotation from "../../assets/ohnotation.png";
 
 const ProjectsPage = () => {
   return (
     <div className="project-content">
       <h2>what I've worked on,</h2>
+
       <div className="project-item">
-        <p>
-          An isometric game engine developed in TypeScript and PixiJS, featuring
-          multiplayer/online functionality!
-        </p>
-        <img src={typeTwo} alt="" className="projectImage" />
+        <h2>Untitled Game</h2>
+        <div className="text-and-image">
+          <p>
+            An isometric game engine developed in TypeScript and PixiJS,
+            featuring multiplayer/online functionality!
+          </p>
+          <img src={typeTwo} alt="" className="projectImage" />
+        </div>
       </div>
 
       <div className="project-item">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero quod
-          in obcaecati et cumque! Illum autem eum natus, corrupti nisi
-          laudantium doloribus necessitatibus sit, asperiores assumenda quia.
-          Minima, quidem cumque.
-        </p>
-        <img src={typeTwo} alt="" className="projectImage" />
-      </div>
-
-      <div className="project-item">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
-          non sed, reiciendis quasi recusandae aliquam voluptate. Quia
-          reiciendis consequuntur, ad laborum itaque, odit quae rem facilis
-          mollitia praesentium, rerum accusamus.
-        </p>
-        <img src={typeTwo} alt="" className="projectImage" />
+        <h2>Oh, Notation</h2>
+        <div className="text-and-image">
+          <p>
+            Welcome to the Oh, Notation! This website is designed to provide an
+            interactive and educational experience to help you understand and
+            master Big O Notation, a critical concept in computer science for
+            analyzing the efficiency of algorithms.
+          </p>
+          <img src={ohNotation} alt="" className="projectImage" />
+        </div>
+        <div className="project-links">
+          <a href="https://github.com/kevinsctfries/ohnotation">GitHub</a>
+          <a href="https://ohnotation.vercel.app/">Visit Site</a>
+        </div>
       </div>
     </div>
   );
