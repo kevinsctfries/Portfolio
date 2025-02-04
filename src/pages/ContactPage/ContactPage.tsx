8a255d8ef6ae98ac34a63a531bc7a14cd3e8a04e
Kevin Fries <kevinsctfries@gmail.com>
2025-02-04 13:14:30 -0500

    Added react fragments to pages

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -2,17 +2,19 @@ import "./contactpage.css";
 
 const ContactPage = () => {
   return (
-    <div className="contact">
-      <h2>contact me</h2>
-      <ul className="contact-info">
-        <li>kevinsctfries@gmail.com</li>
-        <li>
-          <a href="https://www.linkedin.com/in/kevin-fries-097377302">
-            LinkedIn
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="contact">
+        <h2>contact me</h2>
+        <ul className="contact-info">
+          <li>kevinsctfries@gmail.com</li>
+          <li>
+            <a href="https://www.linkedin.com/in/kevin-fries-097377302">
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
