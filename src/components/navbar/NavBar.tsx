310d007b8031058396f9f0457489ddbe821fc32e
Kevin Fries <kevinsctfries@gmail.com>
2025-02-04 16:27:29 -0500

    Fixed flash of unstyled content

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,39 +1,31 @@
 import { IconButton } from "@mui/material";
 import NightlightOutlinedIcon from "@mui/icons-material/NightlightOutlined";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
 
 const NavBar = () => {
   const [darkMode, setDarkMode] = useState<boolean>(() => {
-    // Get the current dark mode preference from localStorage
-    const mode = localStorage.getItem("darkMode");
-    return mode === "enabled";
+    return localStorage.getItem("darkMode") === "enabled";
   });
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("darkmode");
+      document.body.classList.remove("lightmode");
+    } else {
+      document.body.classList.add("lightmode");
+      document.body.classList.remove("darkmode");
+    }
+  }, [darkMode]);
 
   const toggleDarkMode = () => {
     const newDarkMode = !darkMode;
     setDarkMode(newDarkMode);
-
     localStorage.setItem("darkMode", newDarkMode ? "enabled" : "disabled");
-
-    const root = document.documentElement;
-
-    if (newDarkMode) {
-      // Apply dark mode theme dynamically
-      root.style.setProperty("--primary-page-bg", "#191919");
-      root.style.setProperty("--alt-page-bg", "rgba(25, 25, 25, 0.5)");
-      root.style.setProperty("--text-color", "white");
-      root.style.setProperty("--border-color", "#fffef8");
-    } else {
-      // Apply light mode theme dynamically
-      root.style.setProperty("--primary-page-bg", "#fffef8");
-      root.style.setProperty("--alt-page-bg", "#fffef8");
-      root.style.setProperty("--text-color", "black");
-      root.style.setProperty("--border-color", "black");
-    }
   };
+
   return (
     <header>
       <div className="nav-links">
@@ -43,7 +35,7 @@ const NavBar = () => {
       </div>
       <IconButton
         disableRipple={true}
-        size={"small"}
+        size="small"
         id="darkmodeBtn"
         onClick={toggleDarkMode}
         className={darkMode ? "darkBtn" : "lightBtn"}
