6e96ac9154108ac99ce55eed0fe39cfbdd78a438
Kevin Fries <kevinsctfries@gmail.com>
2025-06-14 16:29:46 -0400

    chore(portfolio): update images to so they work on deployment

--- src/pages/ProjectsPage/portfolioRepos.ts
+++ src/pages/ProjectsPage/portfolioRepos.ts
@@ -1,3 +1,9 @@
+import logo from "../../assets/logo.jpeg";
+import ohnotation from "../../assets/ohnotation.png";
+import unet from "../../assets/unet.png";
+import chooseOptionHelper from "../../assets/chooseoptionhelper.png";
+import flashy from "../../assets/flashy.png";
+
 export interface PortfolioRepo {
   name: string;
   desc: string;
@@ -8,31 +14,31 @@ export const portfolioRepos: PortfolioRepo[] = [
   {
     name: "kevinsctfries.github.io",
     desc: "My portfolio site!",
-    image: "/public/logo.jpeg",
+    image: logo,
   },
   {
     name: "ohnotation",
     desc: "A learning resource for understanding time and space complexity",
-    image: "/public/ohnotation.png",
+    image: ohnotation,
   },
   {
     name: "UNet",
     desc: "Reddit-like social platform made from scratch!",
-    image: "/public/unet.png",
+    image: unet,
   },
   {
     name: "ytt-gen",
     desc: "A tool to convert .ASS subtitles to .YTT format",
-    image: "/public/logo.jpeg",
+    image: logo,
   },
   {
     name: "choose-option-helper",
     desc: "A replication of the right-click menu in OSRS",
-    image: "/public/chooseoptionhelper.png",
+    image: chooseOptionHelper,
   },
   {
     name: "flashy",
     desc: "Locally ran AI powered flashcard generator",
-    image: "/public/flashy.png",
+    image: flashy,
   },
 ];
