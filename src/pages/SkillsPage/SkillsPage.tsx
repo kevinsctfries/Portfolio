9f121cd654fa7cf6a1610eb92e9dfff1351c097a
Kevin Fries <kevinsctfries@gmail.com>
2025-06-13 23:11:03 -0400

    feat(SkillsPage): add skillsList.ts to store skills

--- src/pages/SkillsPage/SkillsPage.tsx
+++ src/pages/SkillsPage/SkillsPage.tsx
@@ -1,42 +1,21 @@
 import "./skillspage.scss";
+import {
+  languageSkills,
+  frontEndSkills,
+  backEndSkills,
+  otherSkills,
+} from "./skillsList";
 
 type Skill = {
   name: string;
+  img: string;
 };
 
 const SkillsPage = () => {
-  const frontEndSkills: Skill[] = [
-    { name: "HTML" },
-    { name: "CSS, SCSS, Tailwind" },
-    { name: "JavaScript" },
-    { name: "TypeScript" },
-    { name: "React" },
-    { name: "Angular" },
-    { name: "Redux" },
-  ];
-
-  const backEndSkills: Skill[] = [
-    { name: "MySQL" },
-    { name: "MongoDB" },
-    { name: "Express" },
-    { name: "Node.js" },
-    { name: "Redis" },
-    { name: "Python" },
-  ];
-
-  const otherSkills: Skill[] = [
-    { name: "Git" },
-    { name: "CI/CD Pipelines" },
-    { name: "Jira" },
-    { name: "TensorFlow" },
-    { name: "Pytorch" },
-    { name: "Docker" },
-  ];
-
   const renderSkillCards = (skills: Skill[]) =>
     skills.map((skill, index) => (
       <div className="skill-card" key={index}>
-        <img src="/public/logo.jpeg" alt={skill.name} />
+        <img src={skill.img} alt={skill.name} />
         <span>{skill.name}</span>
       </div>
     ));
@@ -44,15 +23,17 @@ const SkillsPage = () => {
   return (
     <div className="skills-page section">
       <div className="skills-category">
-        <h3>Frontend Skills</h3>
+        <h3>Languages</h3>
+        <div className="skills-grid">{renderSkillCards(languageSkills)}</div>
+      </div>
+      <div className="skills-category">
+        <h3>Front-end Skills</h3>
         <div className="skills-grid">{renderSkillCards(frontEndSkills)}</div>
       </div>
-
       <div className="skills-category">
-        <h3>Backend Skills</h3>
+        <h3>Back-end Skills</h3>
         <div className="skills-grid">{renderSkillCards(backEndSkills)}</div>
       </div>
-
       <div className="skills-category">
         <h3>Other Skills</h3>
         <div className="skills-grid">{renderSkillCards(otherSkills)}</div>
---
+++ src/pages/SkillsPage/skillsList.ts
@@ -0,0 +1,89 @@
+export interface Skill {
+  name: string;
+  img: string;
+}
+
+export const languageSkills: Skill[] = [
+  {
+    name: "JavaScript",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+  },
+  {
+    name: "TypeScript",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+  },
+  {
+    name: "Python",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
+  },
+];
+
+export const frontEndSkills: Skill[] = [
+  {
+    name: "HTML",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-plain-wordmark.svg",
+  },
+  {
+    name: "CSS, SCSS, Tailwind",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+  },
+
+  {
+    name: "React",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+  },
+  {
+    name: "Angular",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg",
+  },
+  {
+    name: "Redux",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg",
+  },
+];
+
+export const backEndSkills: Skill[] = [
+  {
+    name: "MySQL",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg",
+  },
+  {
+    name: "MongoDB",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg",
+  },
+  {
+    name: "Express",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg",
+  },
+  {
+    name: "Node.js",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg",
+  },
+  {
+    name: "Redis",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redis/redis-original.svg",
+  },
+];
+
+export const otherSkills: Skill[] = [
+  {
+    name: "Git",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
+  },
+  {
+    name: "Jira",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jira/jira-original.svg",
+  },
+  {
+    name: "TensorFlow",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tensorflow/tensorflow-original.svg",
+  },
+  {
+    name: "Pytorch",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pytorch/pytorch-original.svg",
+  },
+  {
+    name: "Docker",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg",
+  },
+];
