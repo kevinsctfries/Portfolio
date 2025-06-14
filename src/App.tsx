a45be995736bf98be0fb587808ddf804cb1a8af6
Kevin Fries <kevinsctfries@gmail.com>
2025-06-13 20:24:38 -0400

    feat: add background gridlines

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,25 @@ import HomePage from "./pages/HomePage/HomePage";
 import ProjectsPage from "./pages/ProjectsPage/ProjectsPage";
 import ContactPage from "./pages/ContactPage/ContactPage";
 import SkillsPage from "./pages/SkillsPage/SkillsPage";
+import Background from "./components/background/Background";
 
 function App() {
   return (
-    <div>
-      <div>
-        <NavBar />
-        <section id="home">
-          <HomePage />
-        </section>
-        <section id="skills">
-          <SkillsPage />
-        </section>
-        <section id="projects">
-          <ProjectsPage />
-        </section>
-        <section id="contact">
-          <ContactPage />
-        </section>
-      </div>
+    <div className="app">
+      <Background />
+      <NavBar />
+      <section id="home">
+        <HomePage />
+      </section>
+      <section id="skills">
+        <SkillsPage />
+      </section>
+      <section id="projects">
+        <ProjectsPage />
+      </section>
+      <section id="contact">
+        <ContactPage />
+      </section>
     </div>
   );
 }
---
+++ src/components/background/Background.tsx
@@ -0,0 +1,11 @@
+import "./background.scss";
+
+function Background() {
+  return (
+    <div className="background">
+      <div className="background-grid" />
+    </div>
+  );
+}
+
+export default Background;
