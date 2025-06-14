8062f2952a7a3ba95a1188b4e891b56b7a7dc779
Kevin Fries <kevinsctfries@gmail.com>
2025-06-13 22:13:50 -0400

    style(navbar): improve handleScroll effect for responsiveness and styling

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -6,18 +6,34 @@ const NavBar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
+      const nav = navRef.current;
+      const navLinks = nav?.querySelector<HTMLElement>(".nav-links");
+
+      if (!nav || !navLinks) return;
+
+      const navWidth = nav.offsetWidth;
+      const navLinksWidth = navLinks.offsetWidth;
+
       const { scrollY, innerHeight } = window;
       const { scrollHeight } = document.documentElement;
-      const scrollPercentage = scrollY / (scrollHeight - innerHeight);
-      const width = Math.min(100, 25 + scrollPercentage * 75);
 
-      navRef.current?.style.setProperty("--nav-bg-width", `${width}%`);
+      const min = (navLinksWidth / navWidth) * 100;
+      const max = ((navWidth - 4 * 16) / navWidth) * 100;
+      const percent =
+        min + (scrollY / (scrollHeight - innerHeight)) * (max - min);
+      const width = Math.min(max, Math.max(min, percent));
+
+      nav.style.setProperty("--nav-bg-width", `${width}%`);
     };
 
     handleScroll();
 
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleScroll);
+    };
   }, []);
 
   const scrollToSection = (id: string) => {
@@ -32,9 +48,6 @@ const NavBar = () => {
 
   return (
     <nav ref={navRef}>
-      <div className="initials">
-        <span>KF</span>
-      </div>
       <div className="nav-links">
         <button onClick={() => scrollToSection("home")}>Home</button>
         <button onClick={() => scrollToSection("skills")}>My Skills</button>
