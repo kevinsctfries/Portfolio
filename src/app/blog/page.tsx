333602bdec56c2c759bc5d2ffd79a65ea35010e3
Kevin Fries <kevinsctfries@gmail.com>
2025-06-20 17:47:01 -0400

    feat(blog): add basic blog functionality via Sanity

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,30 @@
-const Blog = () => {
-  return (
-    <div>
-      <h1>This is my blog</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam fugiat
-        ullam minima provident eligendi reiciendis quo excepturi molestiae ex!
-        Quidem, ex ipsam placeat incidunt eligendi rem sed tempore et voluptate!
-      </p>
-    </div>
-  );
-};
+import Link from "next/link";
+import { type SanityDocument } from "next-sanity";
+import { client } from "./client";
 
-export default Blog;
+export const revalidate = 30;
+
+const POSTS_QUERY = `*[
+  _type == "post"
+  && defined(slug.current)
+]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
+
+export default async function IndexPage() {
+  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY);
+
+  return (
+    <main className="">
+      <h1 className="">Posts</h1>
+      <ul className="">
+        {posts.map(post => (
+          <li className="" key={post._id}>
+            <Link href={`/blog/${post.slug.current}`}>
+              <h2 className="">{post.title}</h2>
+              <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
