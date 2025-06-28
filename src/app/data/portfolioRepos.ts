993149bb2a0086e874fc0f54853423ac4c63f0a0
Kevin Fries <kevinsctfries@gmail.com>
2025-06-28 02:34:45 -0400

    add images to portfolio repos

--- src/app/data/portfolioRepos.ts
+++ src/app/data/portfolioRepos.ts
@@ -8,31 +8,31 @@ export const portfolioRepos: PortfolioRepo[] = [
   {
     name: "kevinsctfries.github.io",
     desc: "My portfolio site!",
-    image: "/logo.jpeg",
+    image: "/portfolio.webp",
   },
   {
     name: "ohnotation",
-    desc: "A learning resource for understanding time and space complexity",
-    image: "/ohnotation.png",
+    desc: "A site I made to assist me in learning time and space complexity",
+    image: "/ohnotation.webp",
   },
   {
     name: "UNet",
     desc: "Reddit-like social platform made from scratch!",
-    image: "/unet.png",
+    image: "/unet.webp",
   },
   {
     name: "ytt-gen",
     desc: "A tool to convert .ASS subtitles to .YTT format",
-    image: "/logo.jpeg",
+    image: "/logo.webp",
   },
   {
     name: "choose-option-helper",
     desc: "A replication of the right-click menu in OSRS",
-    image: "/chooseoptionhelper.png",
+    image: "/chooseoptionhelper.webp",
   },
   {
     name: "flashy",
     desc: "Locally ran AI powered flashcard generator",
-    image: "/flashy.png",
+    image: "/flashy.webp",
   },
 ];
