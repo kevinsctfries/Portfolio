f090f1d73c08e5f7cd6dabaab9029ccf3a97e803
Kevin Fries <kevinsctfries@gmail.com>
2025-08-19 17:13:12 -0400

    update repo list with projects

--- src/app/data/portfolioRepos.ts
+++ src/app/data/portfolioRepos.ts
@@ -15,7 +15,25 @@ export const portfolioRepos: PortfolioRepo[] = [
   {
     name: "day-trader",
     desc: "A fun game I made to simulate the fluctuating prices of stocks",
-    image: "/logo.webp",
+    image: "/daytrader.webp",
     link: "daytrader.kevinsctfries.com",
+  },
+  {
+    name: "choose-option-helper",
+    desc: "A tool to simulate the Choose Option menu in OSRS",
+    image: "/chooseoptionhelper.webp",
+    link: "",
+  },
+  {
+    name: "flashy",
+    desc: "A locally ran AI powered flash card and quizzing tool",
+    image: "/flashy.webp",
+    link: "",
+  },
+  {
+    name: "UNet",
+    desc: "A social networking platform inspired by Reddit",
+    image: "/unet.webp",
+    link: "",
   },
 ];
